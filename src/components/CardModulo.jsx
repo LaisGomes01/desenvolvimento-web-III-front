937d6fdd8af7922a290d8322382770d0311419c7
Sirley Addao <sirley.addao@cps.sp.gov.br>
2026-08-18 19:57:33 -0300

    Primeiro componente React

---
+++ src/components/CardModulo.jsx
@@ -0,0 +1,10 @@
+function CardModulo({ titulo, descricao }) {
+  return (
+    <article className="card-modulo">
+      <h2>{titulo}</h2>
+      <p>{descricao}</p>
+    </article>
+  )
+}
+
+export default CardModulo
